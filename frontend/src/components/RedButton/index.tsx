78345ebecd501c6e29df496d187c63f087ff9b60
Caua <cauamistura1@gmail.com>
2023-02-14 11:19:46 -0300

    update proj frontend

---
+++ frontend/src/components/RedButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/icon-button.svg'; 
+import './styles.css';
+
+function RedButton() {
+    return (
+        <div className="red-button" >
+            <img src= {icon} alt="icon-button" />
+        </div>
+    )
+  }
+  
+export default RedButton;
+  
---
+++ frontend/src/models/sale.ts
@@ -0,0 +1,8 @@
+export type Sale = {
+    id: number;
+    sallerName: String;
+    date : String;
+    visited: number;
+    deals: number;
+    amount: number;
+}
