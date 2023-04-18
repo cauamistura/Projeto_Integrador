7c89746b7f0d3f5b70aa93b6f76677ca9196e916
Caua <cauamistura1@gmail.com>
2023-04-18 10:46:08 -0300

    Filtro consulta user

--- Project/src/vision/consultas/VUserCON.java
+++ Project/src/vision/consultas/VUserCON.java
@@ -1,11 +1,13 @@
 package vision.consultas;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -16,17 +18,20 @@ import javax.swing.table.DefaultTableModel;
 import model.MTDadosUser;
 import vision.VMenu;
 import vision.cadastros.VUserCad;
+import vision.padrao.RoundButton;
+import vision.padrao.RoundJTextField;
 
 public class VUserCON extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	private DefaultTableModel model;
 	private JPanel contentPane;
 	private JTable table;
-	private DefaultTableModel model;
-	private JButton btnConfirmar;
-	private JButton btnExcluir;
-	private JTextField edFiltro;
-	private JButton btnFiltro;
+	private JLabel lbFiltro;
+	private RoundJTextField edFiltro;
+	private RoundButton btnConfirmar;
+	private RoundButton btnExcluir;
+	private RoundButton btnFiltro;
 
 	public VUserCON(List<MTDadosUser> dados, VUserCad local) {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -38,12 +43,12 @@ public class VUserCON extends JFrame {
 		contentPane.setLayout(new BorderLayout());
 		setContentPane(contentPane);
 
-		edFiltro = new JTextField();
-		btnFiltro = new JButton("Filtro");
+		edFiltro = new RoundJTextField();
+		edFiltro.setColumns(10);
 		
-		JPanel filterPanel = new JPanel();
-		filterPanel.add(edFiltro);
-		filterPanel.add(btnFiltro);
+		btnFiltro = new RoundButton("Filtro");
+		
+		lbFiltro = new JLabel("Nome: ");
 		
 		JScrollPane scrollPane = new JScrollPane();
 		contentPane.add(scrollPane, BorderLayout.CENTER);
@@ -63,7 +68,7 @@ public class VUserCON extends JFrame {
 
 		table.setModel(model);
 
-		btnConfirmar = new JButton("Confirmar");
+		btnConfirmar = new RoundButton("Confirmar");
 		btnConfirmar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -84,7 +89,7 @@ public class VUserCON extends JFrame {
 			}
 		});
 
-		btnExcluir = new JButton("Excluir");
+		btnExcluir = new RoundButton("Excluir");
 		btnExcluir.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -103,12 +108,33 @@ public class VUserCON extends JFrame {
 			}
 		});
 		
+		btnFiltro.addActionListener(new ActionListener() {	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (edFiltro.getText().isEmpty()) {
+					atualizarTabela(dados, false);
+				} else {
+					atualizarTabela(dados, true);
+				}
+			}
+		});
+
+		JPanel filterPanel = new JPanel();
+		filterPanel.add(lbFiltro);
+		filterPanel.add(edFiltro);
+		filterPanel.add(btnFiltro);
+		
+		JPanel botoes = new JPanel();
+		botoes.add(btnConfirmar);
+		botoes.add(btnExcluir);
+		
 		JPanel buttonsPanel = new JPanel();
-		buttonsPanel.add(filterPanel);
-		buttonsPanel.add(btnConfirmar);
-		buttonsPanel.add(btnExcluir);
+		buttonsPanel.setLayout(new GridLayout(2,1));
+		buttonsPanel.add(filterPanel); 
+		buttonsPanel.add(botoes);
 
 		contentPane.add(buttonsPanel, BorderLayout.SOUTH);
+
 
 
 	}
@@ -120,4 +146,17 @@ public class VUserCON extends JFrame {
 		btnExcluir.setEnabled(false);
 		btnExcluir.setVisible(false);
 	}
+	
+	public void atualizarTabela(List<MTDadosUser> usuarios, Boolean prFiltro) {
+	    model.setRowCount(0);
+
+	    for (MTDadosUser usuario : usuarios) {
+	        if (prFiltro && !usuario.getBDNOMEUSER().toLowerCase().contains(edFiltro.getText().toLowerCase())) {
+	            continue; 
+	        }
+	        Object[] linha = { usuario.getBDCPF(), usuario.getBDNOMEUSER(), usuario.getBDMAIL() };
+	        model.addRow(linha);
+	    }
+	}
+
 }
