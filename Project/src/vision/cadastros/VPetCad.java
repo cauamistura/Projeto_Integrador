05d0f1f81bc5047a9a0d6eb1b68f8b0f2db76007
Davi Augusto Boff <daviboff783@gmail.com>
2023-04-18 12:12:35 -0300

    Desing Pet

--- Project/src/vision/cadastros/VPetCad.java
+++ Project/src/vision/cadastros/VPetCad.java
@@ -1,16 +1,31 @@
 package vision.cadastros;
 
-import java.text.ParseException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.sql.Date;
 
+
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
-import javax.swing.text.MaskFormatter;
 
 import control.DAOTDadosUser;
 import control.DAOTEspecie;
@@ -19,30 +34,14 @@ import control.DAOTRaca;
 import control.DAOTUser;
 import model.MTDadosUser;
 import model.MTEspecie;
-import model.MTEstado;
 import model.MTPet;
 import model.MTRaca;
-import model.MTUser;
+import net.miginfocom.swing.MigLayout;
 import vision.consultas.VPetCON;
-import vision.consultas.VUserCON;
 import vision.padrao.DateTextField;
-
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-
-import java.awt.Font;
-import javax.swing.SwingConstants;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFormattedTextField;
-import java.awt.event.ActionListener;
-import java.sql.Date;
-import java.sql.SQLException;
-import java.awt.event.ActionEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.ItemEvent;
+import vision.padrao.PanelComBackgroundImage;
+import vision.padrao.RoundButton;
+import vision.padrao.RoundJTextField;
 
 public class VPetCad extends JFrame {
 
@@ -58,87 +57,166 @@ public class VPetCad extends JFrame {
 	public DAOTDadosUser FDAOTDadosUser = new DAOTDadosUser();
 	public DAOTRaca FDAOTRaca = new DAOTRaca();
 	
-	private JTextField txtApelidoPet;
-	private JTextField txtNomePet;
-	private DateTextField txtDataNasc;
-	
 	private JPanel contentPane;
-	
-	JComboBox<MTEspecie> especieCb = new JComboBox<MTEspecie>();
-	JComboBox<MTRaca> racaCb = new JComboBox<MTRaca>();
-	JComboBox<MTDadosUser> userCb = new JComboBox<MTDadosUser>();
 	
 	ArrayList<MTRaca> TListRaca = new ArrayList<>();
 	ArrayList<MTEspecie> TListEspecie = new ArrayList<>();
 	ArrayList<MTDadosUser> TListUser = new ArrayList<>();
+	
+	private JTextField txtApelidoPet;
+	private DateTextField txtDataNasc;
+	private JTextField txtNomePet;
+	
+	JComboBox<MTEspecie> especieCb = new JComboBox<MTEspecie>();
+	JComboBox<MTRaca> racaCb = new JComboBox<MTRaca>();
+	JComboBox<MTDadosUser> userCb = new JComboBox<MTDadosUser>();
 
 	/**
 	 * Create the frame.
 	 */
 	public VPetCad() {
+		
+		BufferedImage bg = null;
+		;
+		try {
+			bg = ImageIO.read(new File("src/vision/images/BGLogin.png"));
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 		setTitle("Cadastro de pets");
 
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 319, 512);
+		setBounds(100, 100, 700, 800);
 		contentPane = new JPanel();
 		contentPane.setToolTipText("");
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
-
-		txtNomePet = new JTextField();
-		txtNomePet.setBounds(162, 108, 86, 20);
-		contentPane.add(txtNomePet);
-
-		txtApelidoPet = new JTextField();
-		txtApelidoPet.setBounds(161, 142, 86, 20);
-		contentPane.add(txtApelidoPet);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(158, 174, 255));
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new MigLayout("", "[100px][500px,grow][100px]", "[100px][600px,grow][100px]"));
+		
+		JPanel panel_1 = new PanelComBackgroundImage(bg);
+		panel_1.setBackground(new Color(158, 174, 255));
+		panel.add(panel_1, "cell 1 1,alignx center");
+		panel_1.setLayout(new MigLayout("", "[grow]", "[140.00,grow][20px,grow][95px,grow]"));
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(new Color(125, 137, 245));
+		panel_1.add(panel_5, "cell 0 0,alignx center,growy");
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(VPetCad.class.getResource("/vision/images/doglove 1 (1).png")));
+		panel_5.add(lblNewLabel);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(new Color(125, 137, 245));
+		panel_1.add(panel_3, "cell 0 1,growx");
+		panel_3.setLayout(new MigLayout("", "[10px][150px,grow][10px]", "[][][][][][]"));
+		
+		JLabel lblNewLabel_7 = new JLabel("Cadastro Pet");
+		lblNewLabel_7.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		panel_3.add(lblNewLabel_7, "cell 1 0,alignx center");
+		
+		JLabel lblNewLabel_1 = new JLabel("Espécie:");
+		panel_3.add(lblNewLabel_1, "flowy,cell 1 1");
+		
+		JComboBox especieCb = new JComboBox();
+		panel_3.add(especieCb, "cell 1 1,growx");
+		especieCb.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		JLabel lblNewLabel_2 = new JLabel("Raça:");
+		panel_3.add(lblNewLabel_2, "flowy,cell 1 2");
+		
+		JComboBox racaCb = new JComboBox();
+		panel_3.add(racaCb, "cell 1 2,growx");
+		racaCb.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		JLabel lblNewLabel_3 = new JLabel("Dono:");
+		panel_3.add(lblNewLabel_3, "flowy,cell 1 3");
+		
+		JComboBox uerCb = new JComboBox();
+		uerCb.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		panel_3.add(uerCb, "cell 1 3,growx");
+		
+		JLabel l = new JLabel("Nome:");
+		panel_3.add(l, "flowy,cell 1 4");
+		
+		txtNomePet = new RoundJTextField();
+		txtNomePet.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		panel_3.add(txtNomePet, "cell 1 4,growx");
+		txtNomePet.setColumns(10);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(125, 137, 245));
+		panel_1.add(panel_2, "cell 0 2,grow");
+		panel_2.setLayout(new MigLayout("", "[10px][100px,grow][10px][100px,grow][10px]", "[50px][][80px][50px]"));
+		
+		JLabel i = new JLabel("Apelido(opcional):");
+		panel_2.add(i, "flowy,cell 1 0");
+		
+		txtApelidoPet = new RoundJTextField();
+		txtApelidoPet.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		panel_2.add(txtApelidoPet, "cell 1 0,growx");
 		txtApelidoPet.setColumns(10);
-		txtApelidoPet.setColumns(10);
-
+		
+		JLabel lblNasc = new JLabel("Data de Nasc:");
+		panel_2.add(lblNasc, "flowy,cell 3 0");
 		
 		txtDataNasc = new DateTextField();
-		txtDataNasc.setBounds(162, 177, 86, 20);
-		contentPane.add(txtDataNasc);
+		txtDataNasc.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtDataNasc.setColumns(10);
-
-		JLabel lblNewLabel = new JLabel("Nome:");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel.setBounds(106, 111, 46, 14);
-		contentPane.add(lblNewLabel);
-
-		JLabel lblApelidoopcional = new JLabel("Apelido (opcional):");
-		lblApelidoopcional.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblApelidoopcional.setBounds(39, 145, 114, 14);
-		contentPane.add(lblApelidoopcional);
-
-		JLabel lblDataDeNasc = new JLabel("Data de nasc:");
-		lblDataDeNasc.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblDataDeNasc.setBounds(52, 180, 100, 14);
-		contentPane.add(lblDataDeNasc);
-
-		VPetCad local = this;
-		txtNomePet.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent e) {
-				if (e.getKeyCode() == KeyEvent.VK_F9) {
-					abreConsulta(local);
-				}
+		panel_2.add(txtDataNasc, "cell 3 0,growx");
+		
+		JButton btnLimpar = new RoundButton("Limpar");
+		btnLimpar.setBackground((new Color(255, 199, 0)));
+		btnLimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtApelidoPet.setText("");
+				txtDataNasc.setText("");
+				txtNomePet.setText("");
+				
+				racaCb.setSelectedIndex(0);
+				especieCb.setSelectedIndex(0);
+				userCb.setSelectedIndex(0);
+				
+			}
+		});
+		btnLimpar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		panel_2.add(btnLimpar, "flowx,cell 1 3");
+		
+		JButton btnNewButton = new RoundButton("Consultar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				List<MTPet> lista = FDAOTPet.ListTPet(FDAOTPet);
+				VPetCad prSelf = new VPetCad();
+				VPetCON frame = new VPetCON(lista, prSelf);
+				frame.setLocationRelativeTo(null);
+				frame.setVisible(true);
+				
 			}
 		});
 		
-		JButton btnNewButton = new JButton("Cadastrar");
-		btnNewButton.addActionListener(new ActionListener() {
+		btnNewButton.setBackground((new Color(255, 199, 0)));
+
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		panel_2.add(btnNewButton, "cell 1 3,growx");
+		
+		JButton btnNewButton_1 = new RoundButton("Cadastrar");
+		btnNewButton_1.setBackground((new Color(255, 199, 0)));
+		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-
-
 				if (txtNomePet.getText().isEmpty() || txtNomePet.getText() == null) {
 					JOptionPane.showMessageDialog(null, "Campo vazio: Nome");
 					return;
 				}
 
-				if (txtDataNasc.getText().isEmpty() || txtDataNasc.getText() == null) {
+				if (lblNasc.getText().isEmpty() || lblNasc.getText() == null) {
 					JOptionPane.showMessageDialog(null, "Campo vazio: Data de nascimento");
 					return;
 				}
@@ -174,7 +252,7 @@ public class VPetCad extends JFrame {
 					FDAOTPet.inserir(FDAOTPet);
 					txtNomePet.setText("");
 					txtApelidoPet.setText("");
-					txtDataNasc.setText("");
+					lblNasc.setText("");
 					especieCb.setSelectedIndex(0);
 					racaCb.setSelectedIndex(0);
 					userCb.setSelectedIndex(0);
@@ -185,98 +263,24 @@ public class VPetCad extends JFrame {
 				}
 
 			}
+			
 		});
-		btnNewButton.setBounds(72, 234, 174, 31);
-		contentPane.add(btnNewButton);
-		especieCb.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent e) {
-				racaCb.removeAllItems();
-				
-				racaCb.addItem(null);
-				
-				if (e.getStateChange() == ItemEvent.SELECTED) {
-                    
-                    TListRaca = FDAOTRaca.ListTRaca(FDAOTRaca, achaIdEspecie());
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		panel_2.add(btnNewButton_1, "flowx,cell 3 3,growx");
 
-            		for (MTRaca mtRaca : TListRaca) {
-            			racaCb.addItem(mtRaca);
-            		}
-                }
-			}
-		});
-
-		especieCb.setBounds(164, 6, 87, 22);
-		contentPane.add(especieCb);
+		VPetCad local = this;
 
 		TListEspecie = FDAOTEspecie.ListTEspecie(FDAOTEspecie);
-
-		especieCb.addItem(null);
 		
 		for (MTEspecie mtEspecie : TListEspecie) {
 			especieCb.addItem(mtEspecie);
 		}
-		contentPane.add(especieCb);
 		
 		TListUser = FDAOTDadosUser.ListTDadosUser(FDAOTDadosUser);
-
-		userCb.addItem(null);
 		
 		for (MTDadosUser mtUser : TListUser) {
 			userCb.addItem(mtUser);
 		}
-		contentPane.add(especieCb);
-
-		JLabel lblNewLabel_1 = new JLabel("Espécie:");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_1.setBounds(26, 10, 128, 14);
-		contentPane.add(lblNewLabel_1);
-
-		racaCb.setBounds(164, 48, 86, 22);
-		contentPane.add(racaCb);
-
-		contentPane.add(racaCb);
-		
-		JLabel lblNewLabel_1_1 = new JLabel("Raça:");
-		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_1_1.setBounds(26, 52, 128, 14);
-		contentPane.add(lblNewLabel_1_1);
-		
-		JButton btnNewButton_1 = new JButton("Consultar");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				List<MTPet> lista = FDAOTPet.ListTPet(FDAOTPet);
-				VPetCad prSelf = new VPetCad();
-				VPetCON frame = new VPetCON(lista, prSelf);
-				frame.setLocationRelativeTo(null);
-				frame.setVisible(true);
-			}
-		});
-		btnNewButton_1.setBounds(72, 276, 176, 23);
-		contentPane.add(btnNewButton_1);
-		
-		
-		userCb.setBounds(162, 75, 86, 22);
-		contentPane.add(userCb);
-		
-		JLabel lblNewLabel_1_1_1 = new JLabel("Dono(a):");
-		lblNewLabel_1_1_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel_1_1_1.setBounds(26, 77, 128, 14);
-		contentPane.add(lblNewLabel_1_1_1);
-		
-		JButton btnLimpar = new JButton("Limpar");
-		btnLimpar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				txtApelidoPet.setText("");
-				txtDataNasc.setText("");
-				txtNomePet.setText("");
-				
-				racaCb.setSelectedIndex(0);
-				especieCb.setSelectedIndex(0);
-				userCb.setSelectedIndex(0);
-			}
-		});
-		btnLimpar.setBounds(82, 310, 166, 23);
-		contentPane.add(btnLimpar);
 
 	}
 
