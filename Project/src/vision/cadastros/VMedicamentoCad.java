b152f494a09cec50edeb3a20e454757dd1d6b75f
Davi Boff <daviboff783@gmail.com>
2023-05-02 10:54:15 -0300

    Alterações Desing
    Tela de comorbidade e medicamento alteradas

--- Project/src/vision/cadastros/VMedicamentoCad.java
+++ Project/src/vision/cadastros/VMedicamentoCad.java
@@ -110,7 +110,7 @@ public class VMedicamentoCad extends JFrame {
 		TListMedicacao = FDAOTMedicacao.ListTMedicacao(FDAOTMedicacao);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1300, 800);
+		setBounds(100, 100, 753, 800);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -120,97 +120,92 @@ public class VMedicamentoCad extends JFrame {
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(158, 174, 255));
 		contentPane.add(panel, BorderLayout.CENTER);
-		panel.setLayout(new MigLayout("", "[80px][1000px,grow][80px]", "[100px][600px,grow][100px]"));
+		panel.setLayout(new MigLayout("", "[80px][600px,grow][80px]", "[50px][600px,grow][50px]"));
 		
 		JPanel panel_1 = new PanelComBackgroundImage(bg);
 		panel_1.setBackground(new Color(158, 174, 255));
 		panel.add(panel_1, "cell 1 1,alignx center");
-		panel_1.setLayout(new MigLayout("", "[350px,grow][520px,grow]", "[500px,grow]"));
+		panel_1.setLayout(new MigLayout("", "[350px,grow]", "[600px,grow]"));
 		
 		JPanel panel_3 = new JPanel();
 		panel_3.setBackground(new Color(125, 137, 245));
 		panel_1.add(panel_3, "cell 0 0,grow");
-		panel_3.setLayout(new MigLayout("", "[grow]", "[80px,grow][110px,grow][70px,grow]"));
+		panel_3.setLayout(new MigLayout("", "[grow]", "[70px,grow][300px,grow][25px,grow]"));
 		
 		JPanel panel_6 = new JPanel();
 		panel_6.setBackground(new Color(125, 137, 245));
 		panel_3.add(panel_6, "cell 0 0,grow");
-		panel_6.setLayout(new MigLayout("", "[100px][][100px]", "[]"));
+		panel_6.setLayout(new MigLayout("", "[100px][][100px]", "[][]"));
+		
+		title = new JLabel("Cadastro Medicamento");
+		panel_6.add(title, "cell 1 0");
+		title.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 20));
 		
 		JLabel lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setIcon(new ImageIcon(VMedicamentoCad.class.getResource("/vision/images/med.png")));
-		panel_6.add(lblNewLabel_2, "cell 1 0,alignx center,aligny center");
+		panel_6.add(lblNewLabel_2, "cell 1 1,alignx center,aligny center");
 		
 		JPanel panel_5 = new JPanel();
 		panel_5.setBackground(new Color(125, 137, 245));
 		panel_3.add(panel_5, "cell 0 1,grow");
-		panel_5.setLayout(new MigLayout("", "[grow]", "[][][][][][50px]"));
-		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(new Color(125, 137, 245));
-		panel_1.add(panel_2, "cell 1 0,grow");
-		panel_2.setLayout(new MigLayout("", "[][grow][]", "[40px][180px][100px][250px,grow][100px][100px]"));
-		
-		JScrollPane scrollPane = new JScrollPane();
-		panel_2.add(scrollPane, "cell 1 3,grow");
-		
-		table = new TableSimples(new Object[][] {}, new String[] { "Id", "Medicamento", "Descrição" });
-		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		panel_5.setLayout(new MigLayout("", "[grow]", "[][][50px][130px]"));
 
 		atualizatabela();
-
-		table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
-
-			@Override
-			public void valueChanged(ListSelectionEvent e) {
-				row = table.getSelectedRow();
-				if (row >= 0) {
-
-					// Preencha os campos correspondentes com os dados da linha selecionada
-					nome = table.getValueAt(row, 1).toString();
-					desc = table.getValueAt(row, 2).toString();
-					id = Integer.valueOf(table.getValueAt(row, 0).toString());
-					edNomeMed.setText(nome);
-					edDescMed.setText(desc);
-
-					registroCadastro = false;
-
-					FDAOTMedicacao.setBDIDMEDICACAO(id);
-					
-					lbStatus.setText("Status: Alterando medicamento");
-					
-					
-				}
-			}
-		});
-		scrollPane.setViewportView(table);
 		
 		lbNome = new JLabel("Nome: ");
 		lbNome.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 14));
-		panel_5.add(lbNome, "flowy,cell 0 1");
+		panel_5.add(lbNome, "flowy,cell 0 0");
 		
 		
 		lbDesc = new JLabel("Descrição:");
 		lbDesc.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 14));
-		panel_5.add(lbDesc, "flowy,cell 0 2");
+		panel_5.add(lbDesc, "flowy,cell 0 1");
 		
 		lbStatus= new JLabel("Status: Inserindo Medicamento");
 		lbStatus.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 14));
-		panel_5.add(lbStatus, "cell 0 5");
-		
-		title = new JLabel("Cadastro Medicamento");
-		title.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 20));
-		panel_2.add(title, "cell 1 1,alignx center,aligny top");
+		panel_5.add(lbStatus, "cell 0 2");
 		
 		edNomeMed = new JTextField();
 		edNomeMed.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
-		panel_5.add(edNomeMed, "cell 0 2,growx");
+		panel_5.add(edNomeMed, "cell 0 1,growx");
 		edNomeMed.setColumns(10);
 		
 		edDescMed = new JTextField();
 		edDescMed.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
 		edDescMed.setColumns(10);
-		panel_5.add(edDescMed, "cell 0 1,growx");
+		panel_5.add(edDescMed, "cell 0 0,growx");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel_5.add(scrollPane, "cell 0 3");
+		
+		table = new TableSimples(new Object[][] {}, new String[] { "Id", "Medicamento", "Descrição" });
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+				table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+		
+					@Override
+					public void valueChanged(ListSelectionEvent e) {
+						row = table.getSelectedRow();
+						if (row >= 0) {
+		
+							// Preencha os campos correspondentes com os dados da linha selecionada
+							nome = table.getValueAt(row, 1).toString();
+							desc = table.getValueAt(row, 2).toString();
+							id = Integer.valueOf(table.getValueAt(row, 0).toString());
+							edNomeMed.setText(nome);
+							edDescMed.setText(desc);
+		
+							registroCadastro = false;
+		
+							FDAOTMedicacao.setBDIDMEDICACAO(id);
+							
+							lbStatus.setText("Status: Alterando medicamento");
+							
+							
+						}
+					}
+				});
+				scrollPane.setViewportView(table);
 		
 		JPanel panel_4 = new JPanel();
 		panel_4.setBackground(new Color(125, 137, 245));
