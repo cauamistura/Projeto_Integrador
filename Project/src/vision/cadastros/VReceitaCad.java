98d1002942d9fdbcc0ea78484e180ace718861a3
Vinicius Soares <vini.soares.filho@gmail.com>
2023-05-03 11:45:02 -0300

    Implementação das funções de TReceita
    Correção da tela de Comorbidade e Medicamento

--- Project/src/vision/cadastros/VReceitaCad.java
+++ Project/src/vision/cadastros/VReceitaCad.java
@@ -31,6 +31,8 @@ import model.MTMedicacao;
 import model.MTReceita;
 import model.interfaces.InterMedicamento;
 import net.miginfocom.swing.MigLayout;
+import vision.consultas.VMedicamentoCON;
+import vision.padrao.DateTextField;
 import vision.padrao.PanelComBackgroundImage;
 import vision.padrao.RoundButton;
 import vision.padrao.RoundJTextField;
@@ -54,21 +56,21 @@ public class VReceitaCad extends JFrame implements InterMedicamento{
 	private JPanel panel_3;
 	private JPanel panel_4;
 	private JPanel panel_5;
-	private JLabel lblNewLabel;
-	private JLabel lblNewLabel_1;
-	private lupaButton btnNewButton;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JLabel lblMedicamento;
+	private JLabel lblmedicamento;
+	private lupaButton btnLupa;
+	private DateTextField edDataInicio;
+	private DateTextField edDataFinal;
 	private JTextPane textPane;
-	private JLabel lblNewLabel_2;
-	private JLabel lblNewLabel_3;
-	private JLabel lblNewLabel_4;
-	private JButton btnNewButton_1;
-	private JButton btnNewButton_2;
-	private JButton btnNewButton_3;
+	private JLabel lblDateInicio;
+	private JLabel lblDateFinal;
+	private JLabel lbldesc;
+	private JButton btnLimpar;
+	private JButton btnConf;
+	private JButton btnDeletar;
 	private JLabel lblNewLabel_5;
-	private JLabel lblNewLabel_6;
-	private JLabel lblNewLabel_7;
+	private JLabel title;
+	private JLabel lblStatus;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -90,7 +92,7 @@ public class VReceitaCad extends JFrame implements InterMedicamento{
 			bg = ImageIO.read(new File("src/vision/images/BGLogin.png"));
 
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			//TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -115,25 +117,31 @@ public class VReceitaCad extends JFrame implements InterMedicamento{
 		panel.add(panel_1, "cell 1 1,alignx center");
 		panel_1.setLayout(new MigLayout("", "[grow]", "[][80px,grow][20px,grow][300px,grow][50px,grow]"));
 		
-		lblNewLabel_6 = new JLabel("Cadastro Receita");
-		lblNewLabel_6.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 20));
-		panel_1.add(lblNewLabel_6, "cell 0 0,alignx center");
+		title = new JLabel("Cadastro Receita");
+		title.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 20));
+		panel_1.add(title, "cell 0 0,alignx center");
 		
 		panel_5 = new JPanel();
 		panel_5.setBackground(new Color(125, 137, 245));
 		panel_1.add(panel_5, "cell 0 2,alignx center");
 		panel_5.setLayout(new MigLayout("", "[100px][][][][][][100px]", "[]"));
 		
-		lblNewLabel = new JLabel("Medicamento");
-		lblNewLabel.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 14));
-		panel_5.add(lblNewLabel, "cell 1 0");
+		lblMedicamento= new JLabel("Medicamento");
+		lblMedicamento.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 14));
+		panel_5.add(lblMedicamento, "cell 1 0");
 		
-		btnNewButton = new lupaButton("");
-		panel_5.add(btnNewButton, "cell 3 0");
+		btnLupa = new lupaButton("");
+		btnLupa.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				VMedicamentoCON med = new VMedicamentoCON(FDAOTMedicacao.ListTMedicacao(FDAOTMedicacao), rec);
+				med.setVisible(true);
+			}
+		});
+		panel_5.add(btnLupa, "cell 3 0");
 		
-		lblNewLabel_1 = new JLabel("Paracetamol");
-		lblNewLabel_1.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
-		panel_5.add(lblNewLabel_1, "cell 5 0");
+		lblmedicamento = new JLabel("");
+		lblmedicamento.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		panel_5.add(lblmedicamento, "cell 5 0");
 		
 		panel_4 = new JPanel();
 		panel_4.setBackground(new Color(125, 137, 245));
@@ -141,7 +149,7 @@ public class VReceitaCad extends JFrame implements InterMedicamento{
 		panel_4.setLayout(new MigLayout("", "[100px][][100px]", "[]"));
 		
 		lblNewLabel_5 = new JLabel("");
-//		lblNewLabel_5.setIcon(new ImageIcon(VReceitaCad.class.getResource("/vision/images/medicine.png")));
+		lblNewLabel_5.setIcon(new ImageIcon(VReceitaCad.class.getResource("/vision/images/medicine.png")));
 		panel_4.add(lblNewLabel_5, "cell 1 0");
 		
 		panel_3 = new JPanel();
@@ -149,29 +157,29 @@ public class VReceitaCad extends JFrame implements InterMedicamento{
 		panel_1.add(panel_3, "cell 0 3,alignx center");
 		panel_3.setLayout(new MigLayout("", "[60px][280px,grow][60px]", "[][][100px,grow][]"));
 		
-		lblNewLabel_2 = new JLabel("Data Inicio:");
-		lblNewLabel_2.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
-		panel_3.add(lblNewLabel_2, "flowy,cell 1 0");
+		lblDateInicio = new JLabel("Data Inicio:");
+		lblDateInicio.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		panel_3.add(lblDateInicio, "flowy,cell 1 0");
 		
-		textField = new RoundJTextField();
-		panel_3.add(textField, "cell 1 0,growx");
-		textField.setColumns(10);
+		edDataInicio = new DateTextField();
+		panel_3.add(edDataInicio, "cell 1 0,growx");
+		edDataInicio.setColumns(10);
 		
-		lblNewLabel_3 = new JLabel("Data Final:");
-		lblNewLabel_3.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
-		panel_3.add(lblNewLabel_3, "flowy,cell 1 1");
+		lblDateFinal = new JLabel("Data Final:");
+		lblDateFinal.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		panel_3.add(lblDateFinal, "flowy,cell 1 1");
 		
-		textField_1 = new RoundJTextField();
-		textField_1.setColumns(10);
-		panel_3.add(textField_1, "cell 1 1,growx");
+		edDataFinal = new DateTextField();
+		edDataFinal.setColumns(10);
+		panel_3.add(edDataFinal, "cell 1 1,growx");
 		
-		lblNewLabel_7 = new JLabel("Statu: Insirindo Receita");
-		lblNewLabel_7.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
-		panel_3.add(lblNewLabel_7, "cell 1 3");
+		lblStatus = new JLabel("Status: Aguardando");
+		lblStatus.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		panel_3.add(lblStatus, "cell 1 3");
 		
-		lblNewLabel_4 = new JLabel("Descrição:");
-		lblNewLabel_4.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
-		panel_3.add(lblNewLabel_4, "flowy,cell 1 2");
+		lbldesc = new JLabel("Descrição:");
+		lbldesc.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		panel_3.add(lbldesc, "flowy,cell 1 2");
 		
 		textPane = new JTextPane();
 		panel_3.add(textPane, "cell 1 2,grow");
@@ -181,39 +189,46 @@ public class VReceitaCad extends JFrame implements InterMedicamento{
 		panel_1.add(panel_2, "cell 0 4,alignx center");
 		panel_2.setLayout(new MigLayout("", "[20px][40px][10px][40px][10px][50px][20px]", "[]"));
 		
-		btnNewButton_2 = new RoundButton("Comfirmar");
-		btnNewButton_2.addActionListener(new ActionListener() {
+		btnConf = new RoundButton("Confirmar");
+		btnConf.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				eventConfirmar();
 			}
 		});
-		panel_2.add(btnNewButton_2, "cell 1 0");
+		panel_2.add(btnConf, "cell 1 0");
 		
-		btnNewButton_1 = new RoundButton("Limpar");
-		btnNewButton_1.addActionListener(new ActionListener() {
+		btnLimpar = new RoundButton("Limpar");
+		btnLimpar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				limparDados();
 			}
 		});
-		panel_2.add(btnNewButton_1, "cell 3 0");
+		panel_2.add(btnLimpar, "cell 3 0");
 		
-		btnNewButton_3 = new RoundButton("Deletar");
-		panel_2.add(btnNewButton_3, "cell 5 0");
+		btnDeletar= new RoundButton("Deletar");
+		btnDeletar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 eventDeletar();
+			}
+		});
+		panel_2.add(btnDeletar, "cell 5 0");
 	
 	}
 	private void eventConfirmar() {
 		
-//		if (edDataInicio.getDate().isBefore(edDataFinal.getDate())) {
-//			FDAOTReceita.setBDIDRECEITA(FDAOTReceita.getChaveID("treceita", "BDIDRECEITA"));
-//			FDAOTReceita.setBDIDMEDICACAO(2);
-//			FDAOTReceita.setBDINICIORECEITA(edDataInicio.getDate());
-//			FDAOTReceita.setBDFINALRECEITA(edDataFinal.getDate());
-//			FDAOTReceita.setBDDESCRICAO(textPane.getText());
-//			
-//		}else {
-//			JOptionPane.showMessageDialog(null, "Data Errada seu otario");
-//            
-//            edDataFinal.setText("");
-//            edDataInicio.setText(getName());
-//		}
+		if (edDataInicio.getDate().isBefore(edDataFinal.getDate())) {
+			FDAOTReceita.setBDIDRECEITA(FDAOTReceita.getChaveID("treceita", "BDIDRECEITA"));
+			FDAOTReceita.setBDIDMEDICACAO(2);
+			FDAOTReceita.setBDINICIORECEITA(edDataInicio.getDate());
+			FDAOTReceita.setBDFINALRECEITA(edDataFinal.getDate());
+			FDAOTReceita.setBDDESCRICAO(textPane.getText());
+			
+		}else {
+			JOptionPane.showMessageDialog(null, "Data Errada seu otario");
+            
+            edDataFinal.setText("");
+            edDataInicio.setText(getName());
+		}
 			
 		FDAOTReceita.inserir(FDAOTReceita);
 		
@@ -239,22 +254,22 @@ public class VReceitaCad extends JFrame implements InterMedicamento{
 	}
 	public void med(MTMedicacao med) {
 		
-//		idmedicamento = med.getBDIDMEDICACAO();
-//		lblmedicamento.setText(med.getBDNOMEMEDICACAO());
-//		textPane.setText("\nDescrição do medicamento:\n - "+med.getBDDESCRICAO());
+		idmedicamento = med.getBDIDMEDICACAO();
+		lblmedicamento.setText(med.getBDNOMEMEDICACAO());
+		textPane.setText("\nDescrição do medicamento:\n - "+med.getBDDESCRICAO());
 		
 	}
 	
-//	public void limparDados() {
-//		edDataFinal.setText("");
-//		edDataInicio.setText("");
-//
-//		textPane.setText("");
-//
-//		FDAOTReceita.setBDIDRECEITA(null);
-//		lblmedicamento.setText("");
-//		lblStatus.setText("Status: Inserindo Receita");
-//	}
+	public void limparDados() {
+		edDataFinal.setText("");
+		edDataInicio.setText("");
+
+		textPane.setText("");
+
+		FDAOTReceita.setBDIDRECEITA(null);
+		lblmedicamento.setText("");
+		lblStatus.setText("Status: Inserindo Receita");
+	}
 
 	@Override
 	public void preencheMedicamento(MTMedicacao dados) {
