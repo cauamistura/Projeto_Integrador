59e2de7e5865cbb432b47b31933a981a0dedd0e9
Davi Boff <daviboff783@gmail.com>
2023-06-01 11:50:23 -0300

    Desing saida

--- ClinicaPets/src/main/java/vision/atendimentos/SaidaATE.java
+++ ClinicaPets/src/main/java/vision/atendimentos/SaidaATE.java
@@ -6,9 +6,13 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -32,31 +36,23 @@ import vision.consultas.EntradaCON;
 import vision.consultas.SaidaCON;
 import vision.padrao.CPFTextField;
 import vision.padrao.DateTextField;
+import vision.padrao.PanelComBackgroundImage;
+import vision.padrao.RoundButton;
 import vision.padrao.RoundJTextField;
+import vision.padrao.Util;
 import vision.padrao.lupaButton;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JTextField;
 
 public class SaidaATE extends JFrame implements InterEntrada, InterReceita,InterSaida{
 
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private RoundJTextField edNumEntrada;
-	private RoundJTextField edNomePet;
-	private CPFTextField edCpfUser;
-	private DateTextField edDataSaida;
-	private DateTextField edDataEntrada;
-	private RoundJTextField edNomeUser;
-	private RoundJTextField edRaca;
-	private JTextPane DescSaida;
-	private JButton btnConf;
-	private JButton btnLimpar;
-	private JButton btnExcluir;
-	private JButton btnConsultar;
-	private lupaButton btnEntrada;
-	private lupaButton btnReceita;
-	private JLabel lblNumEntrada;
 	private Boolean EntradaSelecionada = false;
-	private JLabel lblStatus;
 	
 	
 	private DAOAtendimentoSaida FDAOSaida = new DAOAtendimentoSaida();
@@ -66,170 +62,148 @@ public class SaidaATE extends JFrame implements InterEntrada, InterReceita,Inter
 	private EntradaCON FEntradaCON;
 	private boolean existeSaida;
 	private boolean existeReceita; 
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_6;
+	private JTextField textField_7;
 	
 	public SaidaATE() {
+		BufferedImage bg = null;
+		;
+		try {
+			bg = ImageIO.read(new File(Util.getCaminhoIMG("BGLogin.png")));
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 482, 343);
+		setBounds(100, 100, 744, 521);
 		setTitle("Atendimento de Saida");
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(158, 174, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(new MigLayout("", "[50px][600px,grow][50px]", "[50px][350px,grow][50px]"));
 		
-		btnReceita = new lupaButton("Receita");
-		btnReceita.setText("");
-		btnReceita.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				chamaReceitaCad();
-			}			
-		});
-		btnReceita.setBounds(353, 0, 49, 25);
-		contentPane.add(btnReceita);
+		JPanel background = new PanelComBackgroundImage(bg);
+		background.setBackground(new Color(158, 174, 255));
+		contentPane.add(background, "cell 1 1,alignx center");
+		background.setLayout(new MigLayout("", "[grow][200px,grow][grow]", "[grow]"));
 		
-		btnEntrada = new lupaButton("");
-		btnEntrada.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				chamaEntradaCon();
-			}
-		});
-		btnEntrada.setBounds(117, 0, 49, 25);
-		contentPane.add(btnEntrada);
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(125, 137, 245));
+		background.add(panel_2, "cell 0 0,grow");
+		panel_2.setLayout(new MigLayout("", "[grow]", "[grow][grow][]"));
 		
-		DescSaida = new JTextPane();
-		DescSaida.setEditable(false);
-		DescSaida.setBounds(255, 128, 166, 88);
-		contentPane.add(DescSaida);
+		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(new Color(125, 137, 245));
+		panel_2.add(panel_4, "cell 0 0,growx");
+		panel_4.setLayout(new MigLayout("", "[grow]", "[][][][][][][]"));
 		
-		btnConf = new JButton("Confirmar");
-		btnConf.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				eventConfirmar();
-			}
-		});
-		btnConf.setBounds(68, 162, 117, 25);
-		contentPane.add(btnConf);
+		JLabel lblNewLabel_9 = new JLabel("Antendimento Saida");
+		panel_4.add(lblNewLabel_9, "cell 0 0");
 		
-		btnLimpar = new JButton("Limpar");
-		btnLimpar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				eventLimpar();
-			}
-		});
-		btnLimpar.setBounds(68, 199, 117, 25);
-		contentPane.add(btnLimpar);
+		JLabel lblNewLabel = new JLabel("Entrada:");
+		panel_4.add(lblNewLabel, "flowx,cell 0 2");
 		
-		btnExcluir = new JButton("Excluir");
-		btnExcluir.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				eventDelete(FDAOSaida.getBDIDENTRADA());
-			}
-		});
-		btnExcluir.setBounds(68, 236, 117, 25);
-		contentPane.add(btnExcluir);
+		JButton btnNewButton = new lupaButton("");
+		panel_4.add(btnNewButton, "cell 0 2");
 		
-		edNumEntrada = new RoundJTextField();
-		edNumEntrada.setEditable(false);
-		edNumEntrada.setColumns(10);
-		edNumEntrada.setBounds(117, 35, 114, 19);
-		contentPane.add(edNumEntrada);
+		JLabel lblNewLabel_1 = new JLabel("Numero de entrada:");
+		panel_4.add(lblNewLabel_1, "flowy,cell 0 4");
 		
-		edNomePet = new RoundJTextField();
-		edNomePet.setEditable(false);
-		edNomePet.setColumns(10);
-		edNomePet.setBounds(117, 58, 114, 19);
-		contentPane.add(edNomePet);
+		textField = new RoundJTextField();
+		panel_4.add(textField, "cell 0 4,growx");
+		textField.setColumns(10);
 		
-		edCpfUser = new CPFTextField();
-		edCpfUser.setEditable(false);
-		edCpfUser.setColumns(10);
-		edCpfUser.setBounds(117, 89, 114, 19);
-		contentPane.add(edCpfUser);
+		JLabel lblNewLabel_2 = new JLabel("Entrada:");
+		panel_4.add(lblNewLabel_2, "flowy,cell 0 5");
 		
-		edDataSaida = new DateTextField();
-		edDataSaida.setEditable(false);
-		edDataSaida.setColumns(10);
-		edDataSaida.setBounds(117, 137, 114, 19);
-		contentPane.add(edDataSaida);
+		textField_1 = new DateTextField();
+		textField_1.setColumns(10);
+		panel_4.add(textField_1, "cell 0 5,growx");
 		
-		lblNumEntrada = new JLabel("N° Entrada");
-		lblNumEntrada.setBounds(29, 37, 70, 15);
-		contentPane.add(lblNumEntrada);
+		JLabel lblNewLabel_3 = new JLabel("Saida:");
+		panel_4.add(lblNewLabel_3, "flowy,cell 0 6");
 		
-		JLabel lblNomePet = new JLabel("Pet");
-		lblNomePet.setBounds(29, 60, 70, 15);
-		contentPane.add(lblNomePet);
+		textField_2 = new DateTextField();
+		textField_2.setColumns(10);
+		panel_4.add(textField_2, "cell 0 6,growx");
 		
-		JLabel lblUser = new JLabel("Ususario");
-		lblUser.setBounds(29, 91, 70, 15);
-		contentPane.add(lblUser);
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(new Color(125, 137, 245));
+		panel_2.add(panel_3, "cell 0 1,growx");
+		panel_3.setLayout(new MigLayout("", "[100px][30px][100px][0px]", "[][][][][]"));
 		
-		JLabel lblSaida = new JLabel("Saida");
-		lblSaida.setBounds(29, 139, 70, 15);
-		contentPane.add(lblSaida);
+		JButton btnNewButton_1_1 = new RoundButton("Confirmar");
+		panel_3.add(btnNewButton_1_1, "cell 0 0,growx");
 		
-		JLabel lblReceita = new JLabel("Receita");
-		lblReceita.setBounds(265, 5, 70, 15);
-		contentPane.add(lblReceita);
+		JButton btnNewButton_1 = new RoundButton("Limpar");
+		panel_3.add(btnNewButton_1, "cell 2 0,growx");
 		
-		JLabel lblEntrada = new JLabel("Entrada");
-		lblEntrada.setBounds(29, 5, 70, 15);
-		contentPane.add(lblEntrada);
+		JButton btnNewButton_1_2 = new RoundButton("Excluir");
+		panel_3.add(btnNewButton_1_2, "cell 0 2,growx");
 		
-		edNomeUser = new RoundJTextField();
-		edNomeUser.setEditable(false);
-		edNomeUser.setColumns(10);
-		edNomeUser.setBounds(255, 89, 114, 19);
-		contentPane.add(edNomeUser);
+		JButton btnNewButton_1_3 = new RoundButton("Consultar");
+		panel_3.add(btnNewButton_1_3, "cell 2 2,growx");
 		
-		edDataEntrada = new DateTextField();
-		edDataEntrada.setEditable(false);
-		edDataEntrada.setColumns(10);
-		edDataEntrada.setBounds(117, 116, 114, 19);
-		contentPane.add(edDataEntrada);
+		JLabel lblNewLabel_8 = new JLabel("Status: Inserindo uma Saida");
+		panel_2.add(lblNewLabel_8, "cell 0 2");
 		
-		JLabel lblDataEntrada = new JLabel("Entrada");
-		lblDataEntrada.setBounds(29, 118, 70, 15);
-		contentPane.add(lblDataEntrada);
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(125, 137, 245));
+		background.add(panel_1, "cell 1 0,alignx center");
+		panel_1.setLayout(new MigLayout("", "[200px,grow]", "[60px][15px][][][][][150px,grow][50px]"));
 		
-		lblStatus = new JLabel("Status: Inserindo uma Saida");
-		lblStatus.setBounds(29, 283, 135, 13);
-		contentPane.add(lblStatus);
+		JLabel lblNewLabel_7 = new JLabel("Receita:");
+		panel_1.add(lblNewLabel_7, "flowx,cell 0 2");
 		
-		btnConsultar = new JButton("Consultar");
-		btnConsultar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				chamaSaida();
-			}
-		});
-		btnConsultar.setBounds(284, 238, 85, 21);
-		contentPane.add(btnConsultar);
+		JButton btnNewButton_2 = new lupaButton("");
+		panel_1.add(btnNewButton_2, "cell 0 2");
 		
-		edRaca = new RoundJTextField();
-		edRaca.setEditable(false);
-		edRaca.setColumns(10);
-		edRaca.setBounds(255, 58, 114, 19);
-		contentPane.add(edRaca);
+		JLabel lblNewLabel_4 = new JLabel("Pet:");
+		panel_1.add(lblNewLabel_4, "flowy,cell 0 3");
+		
+		textField_3 = new RoundJTextField();
+		textField_3.setColumns(10);
+		panel_1.add(textField_3, "cell 0 3,growx");
+		
+		JLabel lblNewLabel_5 = new JLabel("Usuario:");
+		panel_1.add(lblNewLabel_5, "flowy,cell 0 4");
+		
+		textField_4 = new CPFTextField();
+		textField_4.setColumns(10);
+		panel_1.add(textField_4, "cell 0 4,growx");
+		
+		JLabel lblNewLabel_6 = new JLabel("Descrição:");
+		panel_1.add(lblNewLabel_6, "flowy,cell 0 5");
+		
+		JTextPane textPane = new JTextPane();
+		panel_1.add(textPane, "cell 0 6,grow");
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(125, 137, 245));
+		background.add(panel, "cell 2 0,growx");
+		panel.setLayout(new MigLayout("", "[grow]", "[30px][][][13px][][120px]"));
+		
+		textField_7 = new RoundJTextField();
+		textField_7.setColumns(10);
+		panel.add(textField_7, "cell 0 2,growx");
+		
+		textField_6 = new RoundJTextField();
+		textField_6.setColumns(10);
+		panel.add(textField_6, "cell 0 4,growx");
 	}
 	
 	protected void chamaEntrada() {
 		EntradaATE e = new EntradaATE();
 		e.setVisible(true);
 		
-	}
-
-	private void chamaSaida() {
-		ArrayList<AtendimentoSaida> list = new ArrayList<>();
-		list = FDAOSaida.ListTSaida(FDAOSaida);
-		
-		SaidaCON v = new SaidaCON(list,this);
-		v.setVisible(true);
-	}
-	
-	private void chamaEntradaCon() {
-			list = FDAOEntrada.ListConsulta(FDAOEntrada);
-			FEntradaCON = new EntradaCON(list, this,true,this);
-			FEntradaCON.setVisible(true);
 	}
 	
 	private void chamaReceitaCad() {
@@ -251,43 +225,43 @@ public class SaidaATE extends JFrame implements InterEntrada, InterReceita,Inter
 		DateTimeFormatter FOMATTER = DateTimeFormatter.ofPattern("ddMMyyyy");
 		
 		//Setando text
-		edCpfUser.setText(atendimentos.getBDCPF());
-		edNomeUser.setText(atendimentos.getBDNOMEUSER());
-		edNumEntrada.setText(String.valueOf(atendimentos.getBDIDENTRADA()));
-		edNomePet.setText(atendimentos.getBDNOMEPET());
-		edRaca.setText(atendimentos.getBDNOMERACA());
-		edCpfUser.setText(atendimentos.getBDCPF());
-		edDataEntrada.setText(atendimentos.getBDDATAENT().format(FOMATTER));
+		/*
+		 * edCpfUser.setText(atendimentos.getBDCPF());
+		 * edNomeUser.setText(atendimentos.getBDNOMEUSER());
+		 * edNumEntrada.setText(String.valueOf(atendimentos.getBDIDENTRADA()));
+		 * edNomePet.setText(atendimentos.getBDNOMEPET());
+		 * edRaca.setText(atendimentos.getBDNOMERACA());
+		 * edCpfUser.setText(atendimentos.getBDCPF());
+		 * edDataEntrada.setText(atendimentos.getBDDATAENT().format(FOMATTER));
+		 */
 			
 		//Setando DAOSaida
 		FDAOSaida.setBDCOMORBIDADE(atendimentos.getBDCOMORBIDADE());
 		FDAOSaida.setBDIDENTRADA(atendimentos.getBDIDENTRADA());
 		FDAOSaida.setBDIDPET(atendimentos.getBDIDPET());
 			
-		edDataSaida.setEditable(true);
-		DescSaida.setEditable(true);
+		/*
+		 * edDataSaida.setEditable(true); DescSaida.setEditable(true);
+		 */
 			
 		EntradaSelecionada = true;
-		btnExcluir.setEnabled(false);
+		/* btnExcluir.setEnabled(false); */
 				
 	}
 	public void travaCliente() {
-		btnEntrada.setEnabled(false);
-		btnReceita.setEnabled(false);
-		
-		btnExcluir.setVisible(false);
-		btnLimpar.setVisible(false);
-		btnConsultar.setVisible(false);
-		
-		edNumEntrada.setEnabled(false);
-		edCpfUser.setEnabled(false);
-		
-		edDataEntrada.setEnabled(false);
-		edDataSaida.setEnabled(false);
-		edNomePet.setEnabled(false);
-		DescSaida.setEnabled(false);
-		
-		lblStatus.setVisible(false);
+		/*
+		 * btnEntrada.setEnabled(false); btnReceita.setEnabled(false);
+		 * 
+		 * btnExcluir.setVisible(false); btnLimpar.setVisible(false);
+		 * btnConsultar.setVisible(false);
+		 * 
+		 * edNumEntrada.setEnabled(false); edCpfUser.setEnabled(false);
+		 * 
+		 * edDataEntrada.setEnabled(false); edDataSaida.setEnabled(false);
+		 * edNomePet.setEnabled(false); DescSaida.setEnabled(false);
+		 * 
+		 * lblStatus.setVisible(false);
+		 */
 	}
 	
 	private void preecheDadosSaida(AtendimentoSaida atendimentos) {
@@ -296,15 +270,16 @@ public class SaidaATE extends JFrame implements InterEntrada, InterReceita,Inter
 		DateTimeFormatter FOMATTER = DateTimeFormatter.ofPattern("ddMMyyyy");
 		
 		//Setando text
-		edNumEntrada.setText(String.valueOf(atendimentos.getBDIDENTRADA()));
-		edCpfUser.setText(atendimentos.getBDCPF());
-		edNomeUser.setText(atendimentos.getBDNOMEUSER());
-		edNomePet.setText(atendimentos.getBDNOMEPET());
-		edRaca.setText(atendimentos.getBDNOMERACA());
-		edDataEntrada.setText(atendimentos.getBDDATAENT().format(FOMATTER));
-		edDataSaida.setText(atendimentos.getBDDATASAIDA().format(FOMATTER));
-		DescSaida.setText(atendimentos.getBDDESC());
-			
+		/*
+		 * edNumEntrada.setText(String.valueOf(atendimentos.getBDIDENTRADA()));
+		 * edCpfUser.setText(atendimentos.getBDCPF());
+		 * edNomeUser.setText(atendimentos.getBDNOMEUSER());
+		 * edNomePet.setText(atendimentos.getBDNOMEPET());
+		 * edRaca.setText(atendimentos.getBDNOMERACA());
+		 * edDataEntrada.setText(atendimentos.getBDDATAENT().format(FOMATTER));
+		 * edDataSaida.setText(atendimentos.getBDDATASAIDA().format(FOMATTER));
+		 * DescSaida.setText(atendimentos.getBDDESC());
+		 */
 		//Setando DAOSaida
 		FDAOSaida.setBDIDRECEITA(atendimentos.getBDIDRECEITA());
 		FDAOSaida.setBDCOMORBIDADE(atendimentos.getBDCOMORBIDADE());
@@ -315,71 +290,23 @@ public class SaidaATE extends JFrame implements InterEntrada, InterReceita,Inter
 		EntradaSelecionada = true;
 		existeSaida = true;
 			
-		edDataSaida.setEditable(true);
-		DescSaida.setEditable(true);
-			
-	}
-	
-	private void eventConfirmar() {
-	
-		if(EntradaSelecionada) {
-			
-			if(existeReceita) {
-				FDAOSaida.setBDIDSAIDA(FDAOSaida.getChaveID("TAtendimento_Saida", "BDIDSAIDA"));
-				FDAOSaida.setBDDESC(DescSaida.getText());
-				FDAOSaida.setBDDATASAIDA(edDataSaida.getDate());
-
-				if(existeSaida) {
-					FDAOReceita.alterar(FDAOReceita);
-					FDAOSaida.alterar(FDAOSaida);
-					
-					JOptionPane.showMessageDialog(null, "Dados alterados com SUCESSO!!");
-					
-				}else {
-					FDAOReceita.inserir(FDAOReceita);
-					FDAOSaida.inserir(FDAOSaida);
-					
-					btnExcluir.setEnabled(true);
-					
-					JOptionPane.showMessageDialog(null, "Dados inseridos com SUCESSO!!");
-				}
-				
-				int resposta = JOptionPane.showConfirmDialog(null,
-						"Deseja deletar os dados presentes na Tela?",
-						"Confirmação", JOptionPane.YES_NO_OPTION);
-
-				if (resposta == JOptionPane.YES_OPTION) {
-					eventLimpar();
-				}
-			}else {
-				JOptionPane.showMessageDialog(null, "Cadastre uma receita antes de cadastrar uma saida");
-				chamaReceitaCad();
-			}
-			
-			
-		
-		}else {
-			JOptionPane.showMessageDialog(null, "Selecione um  atendimento antes de confirmar a ação");
-		}	
-		
-			
+		/*
+		 * edDataSaida.setEditable(true); DescSaida.setEditable(true);
+		 */
 	}
 	
 	private void eventLimpar() {
-		edCpfUser.setText("");
-		edDataEntrada.setText("");
-		edDataSaida.setText("");
-		edNomePet.setText("");
-		edNomeUser.setText("");
-		edNumEntrada.setText("");
-		DescSaida.setText("");
-		edRaca.setText("");
-		
+		/*
+		 * edCpfUser.setText(""); edDataEntrada.setText(""); edDataSaida.setText("");
+		 * edNomePet.setText(""); edNomeUser.setText(""); edNumEntrada.setText("");
+		 * DescSaida.setText(""); edRaca.setText("");
+		 */
 		FDAOSaida.setBDIDENTRADA(null);
 		FDAOReceita.setBDIDRECEITA(null);;
 		
-		edDataSaida.setEditable(false);
-		DescSaida.setEditable(false);
+		/*
+		 * edDataSaida.setEditable(false); DescSaida.setEditable(false);
+		 */
 		
 		EntradaSelecionada = false;
 		existeSaida = false;
