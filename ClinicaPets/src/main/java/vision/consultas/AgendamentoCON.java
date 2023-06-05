9149ded390aaea41a570f3af65fd74ca6d0bdb84
Davi Augusto Boff <daviboff783@gmail.com>
2023-06-04 23:01:03 -0300

    Tela AgendamentoCon

--- ClinicaPets/src/main/java/vision/consultas/AgendamentoCON.java
+++ ClinicaPets/src/main/java/vision/consultas/AgendamentoCON.java
@@ -1,10 +1,14 @@
 package vision.consultas;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,9 +25,17 @@ import model.Agendamento;
 import model.DadosUser;
 import model.interfaces.InterAgendamento;
 import vision.padrao.DateTextField;
+import vision.padrao.PanelComBackgroundImage;
 import vision.padrao.RoundButton;
+import vision.padrao.RoundJTextField;
 import vision.padrao.TableSimples;
 import vision.padrao.Util;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import java.awt.Font;
 
 public class AgendamentoCON extends JFrame {
 	
@@ -31,74 +43,78 @@ public class AgendamentoCON extends JFrame {
 	
 	public DAOAgendamento FDAOTAgendamento = new DAOAgendamento();
 	private JPanel contentPane;
-	private TableSimples table;
-	private JLabel lbFiltro;
-	private DateTextField edData;
-	private RoundButton btnConfirmar;
-	private RoundButton btnExcluir;
-	private RoundButton btnFiltro;
 	private ArrayList<Agendamento> dados;
 	
 	private DAODadosUser user = new DAODadosUser();
 	private ArrayList<DadosUser> listUser = user.ListTDadosUser(user);
+	private TableSimples table;
+	private RoundButton btnConfirmar;
+	private RoundButton btnExcluir;
+	private RoundButton btnFiltro;
+	private DateTextField edData;
 	
 	public AgendamentoCON(ArrayList<Agendamento> dadosConstr, InterAgendamento event) {
 		
+setIconImage(Toolkit.getDefaultToolkit().getImage(Util.getCaminhoIMG("logo.png")));
+		
+		setTitle("Consulta de Pet");
+		BufferedImage bg = null;
+		;
+		try {
+			bg = ImageIO.read(new File(Util.getCaminhoIMG("BGLogin.png")));
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Util.getCaminhoIMG("logo.png")));
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 616, 372);
+		setBounds(100, 100, 650, 451);
 		setTitle("Consulta de Agendamento");
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
-		contentPane.setLayout(new BorderLayout());
+		contentPane.setBackground(new Color(158, 174, 255));
 		setContentPane(contentPane);
-
-		edData = new DateTextField();
-		edData.setColumns(10);
-
-		lbFiltro = new JLabel("Data:");
-
+		contentPane.setLayout(new MigLayout("", "[50px][525px,grow][50px]", "[50px][350px,grow][50px]"));
+		
+		JPanel panelBackground = new PanelComBackgroundImage(bg);
+		panelBackground.setBackground(new Color(158, 174, 255));
+		contentPane.add(panelBackground, "cell 1 1,alignx center");
+		panelBackground.setLayout(new MigLayout("", "[grow]", "[250px,grow][grow][grow]"));
+		
+		JPanel panelTabela = new JPanel();
+		panelTabela.setBackground(new Color(125, 137, 245));
+		panelBackground.add(panelTabela, "cell 0 0,growx");
+		panelTabela.setLayout(new MigLayout("", "[][grow][]", "[][450px,grow]"));
+		
+		JLabel lblNewLabel = new JLabel("Consulta Agendamento");
+		lblNewLabel.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 15));
+		panelTabela.add(lblNewLabel, "cell 1 0,alignx center");
+		
 		JScrollPane scrollPane = new JScrollPane();
-		contentPane.add(scrollPane, BorderLayout.CENTER);
-
+		panelTabela.add(scrollPane, "cell 1 1,growx");
+		
 		table = new TableSimples(new Object[][] {}, new String[] { "Número", "Dono", "Pet", "Data", "Hora" });
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		
 		scrollPane.setViewportView(table);
 		
-		btnConfirmar = new RoundButton("Confirmar");
-		btnConfirmar.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				int[] selectedRows = table.getSelectedRows();
-				for (int i = 0; i < selectedRows.length; i++) {
-					int modelIndex = table.convertRowIndexToModel(selectedRows[i]);	
-					Agendamento dado = dados.get(modelIndex);
-					event.preencheAge(dado);
-					dispose();
-				}
-				table.setCellSelectionEnabled(true);
-			}
-		});
-
-		btnExcluir = new RoundButton("Excluir");
-		btnExcluir.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				int[] selectedRows = table.getSelectedRows();
-				for (int i = 0; i < selectedRows.length; i++) {
-					int modelIndex = table.convertRowIndexToModel(selectedRows[i]);	
-					Agendamento dado = dados.get(modelIndex);
-					event.excluiAge(dado.getId());
-					dispose();
-				}
-			}
-		});
+		JPanel panelFiltro = new JPanel();
+		panelFiltro.setBackground(new Color(125, 137, 245));
+		panelBackground.add(panelFiltro, "cell 0 1,grow");
+		panelFiltro.setLayout(new MigLayout("", "[100px][][100px,grow][][][100px]", "[]"));
 		
-		btnFiltro = new RoundButton("Filtro");
+		JLabel lbFiltro = new JLabel("Data:");
+		lbFiltro.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 14));
+		panelFiltro.add(lbFiltro, "cell 1 0");
+		
+		edData = new DateTextField();
+		edData.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 12));
+		panelFiltro.add(edData, "cell 2 0,growx");
+		edData.setColumns(10);
+		
+		JButton btnFiltro = new RoundButton("Filtro");
 		btnFiltro.addActionListener(new ActionListener() {
-			@Override
 			public void actionPerformed(ActionEvent e) {
 				if(edData.validaDate()) {
 					dados = FDAOTAgendamento.ListCon(edData.getDate(), true);
@@ -110,35 +126,56 @@ public class AgendamentoCON extends JFrame {
 			
 				table.setRowSelectionInterval(0, 0);
 			}	
+		});
+		btnFiltro.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 12));
+		panelFiltro.add(btnFiltro, "cell 4 0");
+		
+		JPanel panelBotoes = new JPanel();
+		panelBotoes.setBackground(new Color(125, 137, 245));
+		panelBackground.add(panelBotoes, "cell 0 2,grow");
+		panelBotoes.setLayout(new MigLayout("", "[160px][100px][40px][100px][100px]", "[]"));
+		
+		JButton btnConfirmar = new RoundButton("Confirmar");
+		btnConfirmar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int[] selectedRows = table.getSelectedRows();
+				for (int i = 0; i < selectedRows.length; i++) {
+					int modelIndex = table.convertRowIndexToModel(selectedRows[i]);	
+					Agendamento dado = dados.get(modelIndex);
+					event.preencheAge(dado);
+					dispose();
+				}
+				table.setCellSelectionEnabled(true);
+			}
+		});
+		btnConfirmar.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 12));
+		panelBotoes.add(btnConfirmar, "cell 1 0,growx");
+		
+		JButton btnExcluir = new RoundButton("Excluir");
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int[] selectedRows = table.getSelectedRows();
+				for (int i = 0; i < selectedRows.length; i++) {
+					int modelIndex = table.convertRowIndexToModel(selectedRows[i]);	
+					Agendamento dado = dados.get(modelIndex);
+					event.excluiAge(dado.getId());
+					dispose();
+				}
+			}
 			
 		});
-
-		JPanel filterPanel = new JPanel();
-		filterPanel.add(lbFiltro);
-		filterPanel.add(edData);
-		filterPanel.add(btnFiltro);
-
-		JPanel botoes = new JPanel();
-		botoes.add(btnConfirmar);
-		botoes.add(btnExcluir);
-
-		JPanel buttonsPanel = new JPanel();
-		buttonsPanel.setLayout(new GridLayout(2, 1));
-		buttonsPanel.add(filterPanel);
-		buttonsPanel.add(botoes);
-
-		contentPane.add(buttonsPanel, BorderLayout.SOUTH);
+		btnExcluir.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 12));
+		panelBotoes.add(btnExcluir, "cell 3 0,growx");
 		
 		dados = dadosConstr;
 		atualizarTabela(dados);
 	}
 	
 	public void atualizarTabela(List<Agendamento> com) {
-		table.limparTabela();
 		DateTimeFormatter FOMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		for (Agendamento agendamento: com) {			
 			Object[][] rowData = {{ agendamento.getId(), getUser(agendamento.getBDIDUSER()), agendamento.getBDNOMEPET(), agendamento.getDateAgendamento().format(FOMATTER), agendamento.getHora() }};
-			table.preencherTabela(rowData);	
+			table.preencherTabela(rowData); 
 		}
 	}
 	
